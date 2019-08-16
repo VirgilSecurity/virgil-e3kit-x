d2f445cf3facc350c0b8945670da5df6551ed588
Oleksandr Deundiak <deundiak@gmail.com>
2019-08-16 13:06:31 +0300

    Moved LookupResult back to EThree class

--- Source/Managers/LookupManager.swift
+++ Source/Managers/LookupManager.swift
@@ -35,13 +35,9 @@
 //
 
 import VirgilSDK
-import VirgilCrypto
 
 /// Typealias for the result of findUsers call
 public typealias FindUsersResult = [String: Card]
-
-/// Typealias for the valid result of lookupPublicKeys call
-public typealias LookupResult = [String: VirgilPublicKey]
 
 internal class LookupManager {
     internal let cardStorage: SQLiteCardStorage
