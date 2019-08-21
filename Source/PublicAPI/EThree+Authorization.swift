4511d340c074c5a91b1d44a062b5830aa7243086
Evgeny Pyvovarov <evgenypyvovarov@gmail.com>
2019-08-21 21:56:49 +0300

    Refactor

--- Source/PublicAPI/EThree+Authorization.swift
+++ Source/PublicAPI/EThree+Authorization.swift
@@ -73,38 +73,11 @@ extension EThree {
         }
     }
 
-    /// Generates new Private Key, publishes Card on Virgil Cards Service and saves Private Key in local storage
+    /// Publishes Card on Virgil Cards Service and saves Private Key in local storage
     ///
+    /// - Parameter keyPair: `VirgilKeyPair` to publish Card with. Will generate if not specified
     /// - Returns: CallbackOperation<Void>
-    public func register() -> GenericOperation<Void> {
-        return CallbackOperation { _, completion in
-            self.queue.async {
-                do {
-                    guard try !self.localKeyStorage.exists() else {
-                        throw EThreeError.privateKeyExists
-                    }
-
-                    let cards = try self.cardManager.searchCards(identities: [self.identity]).startSync().get()
-
-                    guard cards.isEmpty else {
-                        throw EThreeError.userIsAlreadyRegistered
-                    }
-
-                    try self.publishCardThenSaveLocal()
-
-                    completion((), nil)
-                } catch {
-                    completion(nil, error)
-                }
-            }
-        }
-    }
-
-    /// Uses provided Private Key to publish Card to Virgil Cards Service. Saves Private Key in local storage
-    ///
-    /// - Parameter keyPair: Key Pair to publish Card with
-    /// - Returns: CallbackOperation<Void>
-    public func register(with keyPair: VirgilKeyPair) -> GenericOperation<Void> {
+    public func register(with keyPair: VirgilKeyPair? = nil) -> GenericOperation<Void> {
         return CallbackOperation { _, completion in
             self.queue.async {
                 do {
