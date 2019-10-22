184e99ca9b0cef9e9415eaeadf0debc5ce622d09
Eugen Pivovarov <epivovarov@virgilsecurity.com>
2019-10-22 15:23:16 +0300

    Added auth encryption, deprecated old encrypt

---
+++ Source/PublicAPI/Deprecated/EThree+Lookup.swift
@@ -0,0 +1,75 @@
+//
+// Copyright (C) 2015-2019 Virgil Security Inc.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     (1) Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//     (2) Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//     (3) Neither the name of the copyright holder nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+//
+
+import VirgilSDK
+
+// MARK: - Extension with deprecated lookup methods
+public extension EThree {
+    /// Retrieves users public keys from the Virgil Cloud
+    ///
+    /// - Parameter identities: array of identities to find
+    /// - Returns: CallbackOperation<LookupResult>
+    @available(*, deprecated, message: "Use findUsers instead.")
+    func lookupPublicKeys(of identities: [String]) -> GenericOperation<LookupResult> {
+        return CallbackOperation { _, completion in
+            do {
+                let cards = try self.findUsers(with: identities, forceReload: true).startSync().get()
+
+                let result = cards.mapValues { $0.publicKey }
+
+                completion(result, nil)
+            }
+            catch {
+                completion(nil, error)
+            }
+
+        }
+    }
+
+    /// Retrieves users public keys from the Virgil Cloud
+    ///
+    /// - Parameters:
+    ///   - identities: array of identities to find
+    ///   - completion: completion handler
+    ///   - lookupResult: dictionary with idenities as keys and found keys as values
+    ///   - error: corresponding error
+    @available(*, deprecated, message: "Use findUsers instead.")
+    @objc func lookupPublicKeys(of identities: [String],
+                                completion: @escaping (_ lookupResult: LookupResult?,
+                                                       _ error: Error?) -> Void) {
+        self.lookupPublicKeys(of: identities).start(completion: completion)
+    }
+}
---
+++ Source/PublicAPI/Encryption/EThree+Streams.swift
@@ -0,0 +1,153 @@
+//
+// Copyright (C) 2015-2019 Virgil Security Inc.
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+//     (1) Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//
+//     (2) Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in
+//     the documentation and/or other materials provided with the
+//     distribution.
+//
+//     (3) Neither the name of the copyright holder nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+//
+
+import VirgilSDK
+import VirgilCrypto
+
+// MARK: - Extension with streams encrypt and decrypt operations
+extension EThree {
+    /// Signs then encrypts stream and signature for user
+    ///
+    /// - Parameters:
+    ///   - stream: data stream to be encrypted
+    ///   - outputStream: stream with encrypted data
+    ///   - user: user Card to encrypt for
+    /// - Throws: corresponding error
+    /// - Important: Automatically includes self key to recipientsKeys.
+    /// - Important: Requires private key in local storage
+    @objc(authEncryptStream:withSize:toStream:forUser:error:)
+    open func authEncrypt(_ stream: InputStream,
+                          streamSize: Int,
+                          to outputStream: OutputStream,
+                          for user: Card) throws {
+        try self.authEncrypt(stream, streamSize: streamSize, to: outputStream, for: [user.identity: user])
+    }
+
+    /// Signs then encrypts stream and signature for users
+    ///
+    /// - Parameters:
+    ///   - stream: data stream to be encrypted
+    ///   - outputStream: stream with encrypted data
+    ///   - users: result of findUsers call recipient Cards with Public Keys to sign and encrypt with.
+    ///            Use nil to sign and encrypt for self
+    /// - Throws: corresponding error
+    /// - Important: Automatically includes self key to recipientsKeys.
+    /// - Important: Requires private key in local storage
+    /// - Note: Avoid key duplication
+    @objc(authEncryptStream:withSize:toStream:forUsers:error:)
+    open func authEncrypt(_ stream: InputStream,
+                          streamSize: Int,
+                          to outputStream: OutputStream,
+                          for users: FindUsersResult? = nil) throws {
+        try self.encryptInternal(stream, streamSize: streamSize, to: outputStream, for: users?.map { $1.publicKey })
+    }
+
+    /// Decrypts stream and signature and verifies signature of sender
+    ///
+    /// - Parameters:
+    ///   - stream: stream with encrypted data
+    ///   - outputStream: stream with decrypted data
+    ///   - user: sender Card with Public Key to verify with. Use nil to decrypt and verify from self.
+    /// - Throws: corresponding error
+    /// - Important: Requires private key in local storage
+    @objc open func authDecrypt(_ stream: InputStream, to outputStream: OutputStream, from user: Card? = nil) throws {
+        try self.decryptInternal(stream, to: outputStream, from: nil)
+    }
+
+    /// Decrypts stream and signature and verifies signature of sender
+    ///
+    /// - Parameters:
+    ///   - stream: stream with encrypted data
+    ///   - outputStream: stream with decrypted data
+    ///   - user: sender Card with Public Key to verify with
+    ///   - date: date of encryption to use proper card version
+    /// - Throws: corresponding error
+    /// - Important: Requires private key in local storage
+    @objc open func authDecrypt(_ stream: InputStream,
+                                to outputStream: OutputStream,
+                                from user: Card,
+                                date: Date) throws {
+        var card = user
+
+        while let previousCard = card.previousCard {
+            guard card.createdAt > date else {
+                break
+            }
+
+            card = previousCard
+        }
+
+        try self.decryptInternal(stream, to: outputStream, from: card.publicKey)
+    }
+}
+
+extension EThree {
+    internal func encryptInternal(_ stream: InputStream,
+                                  streamSize: Int,
+                                  to outputStream: OutputStream,
+                                  for publicKeys: [VirgilPublicKey]?) throws {
+        let selfKeyPair = try self.localKeyStorage.retrieveKeyPair()
+
+        var pubKeys = [selfKeyPair.publicKey]
+
+        if let publicKeys = publicKeys {
+            guard !publicKeys.isEmpty else {
+                throw EThreeError.missingPublicKey
+            }
+
+            pubKeys += publicKeys
+        }
+
+        try self.crypto.authEncrypt(stream,
+                                    streamSize: streamSize,
+                                    to: outputStream,
+                                    with: selfKeyPair.privateKey,
+                                    for: pubKeys)
+    }
+
+    internal func decryptInternal(_ stream: InputStream,
+                                  to outputStream: OutputStream,
+                                  from publicKey: VirgilPublicKey?) throws {
+        let selfKeyPair = try self.localKeyStorage.retrieveKeyPair()
+
+        let publicKey = publicKey ?? selfKeyPair.publicKey
+
+        try self.crypto.authDecrypt(stream,
+                                    to: outputStream,
+                                    with: selfKeyPair.privateKey,
+                                    usingOneOf: [publicKey])
+    }
+}
