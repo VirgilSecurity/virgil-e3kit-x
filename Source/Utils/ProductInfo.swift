545527282d3332a09a67e823517641f938c19724
Oleksandr Deundiak <deundiak@gmail.com>
2020-02-11 13:40:52 +0200

    Version bump fix

--- Source/Utils/ProductInfo.swift
+++ Source/Utils/ProductInfo.swift
@@ -38,5 +38,5 @@ import Foundation
 
 internal enum ProductInfo {
     internal static let name: String = "e3kit"
-    internal static let version: String = "0.8.0"
+    internal static let version: String = "2.0.0"
 }
