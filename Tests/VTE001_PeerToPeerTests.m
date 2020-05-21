3821ce06a08effeaedc5a4846f2bfa9484c95811
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2020-05-21 19:14:47 +0300

    Added streams compatibility test

--- Tests/VTE001_PeerToPeerTests.m
+++ Tests/VTE001_PeerToPeerTests.m
@@ -50,7 +50,7 @@
     [super tearDown];
 }
 
-- (void)test01_STE_3 {
+- (void)test001_STE_3 {
     XCTestExpectation *ex = [self expectationWithDescription:@"Simple encrypt decrypt should success"];
 
     [self.eThree registerWithCompletion:^(NSError *error) {
@@ -97,7 +97,7 @@
     }];
 }
 
-- (void)test02_STE_4 {
+- (void)test002_STE_4 {
     XCTestExpectation *ex = [self expectationWithDescription:@"Encrypt for empty array of keys should throw error"];
 
     [self.eThree registerWithCompletion:^(NSError *error) {
@@ -116,7 +116,7 @@
     }];
 }
 
-- (void)test03_STE_5 {
+- (void)test003_STE_5 {
     XCTestExpectation *ex = [self expectationWithDescription:@"Should throw error if decrypted text is not verified"];
 
     [self.eThree registerWithCompletion:^(NSError *error) {
@@ -151,7 +151,7 @@
     }];
 }
 
-- (void)test04_STE_6 {
+- (void)test004_STE_6 {
     NSError *error;
     [self.keychainStorage deleteEntryWithName:self.eThree.identity queryOptions:nil error:nil];
 
@@ -169,7 +169,7 @@
     XCTAssert(decrypted == nil);
 }
 
-- (void)test05_STE_22 {
+- (void)test005_STE_22 {
     XCTestExpectation *ex = [self expectationWithDescription:@"Should encrypt then decrypt streams"];
 
     [self.eThree registerWithCompletion:^(NSError *error) {
@@ -211,7 +211,7 @@
     }];
 }
 
-- (void)test06_STE_40 {
+- (void)test006_STE_40 {
     XCTestExpectation *ex = [self expectationWithDescription:@"Decrypt text, which was encrypted with old card"];
 
     [self.eThree registerWithCompletion:^(NSError *error) {
@@ -295,7 +295,7 @@
     }];
 }
 
-- (void)test07_STE_41 {
+- (void)test007_STE_41 {
     XCTestExpectation *ex = [self expectationWithDescription:@"Simple encrypt decrypt with deprecated methods"];
 
     [self.eThree registerWithCompletion:^(NSError *error) {
@@ -340,7 +340,7 @@
     }];
 }
 
-- (void)test08_STE_71 {
+- (void)test008_STE_71 {
     XCTestExpectation *ex = [self expectationWithDescription:@"Simple encrypt decrypt with deprecated methods should succeed"];
 
     [self.eThree registerWithCompletion:^(NSError *error) {
@@ -387,7 +387,7 @@
     }];
 }
 
-- (void)test09_STE_88 {
+- (void)test009_STE_88 {
     XCTestExpectation *ex = [self expectationWithDescription:@"Decrypt stream, encrypted and signed as data should succeed"];
 
     [self.eThree registerWithCompletion:^(NSError *error) {
@@ -433,6 +433,35 @@
         if (error != nil)
             XCTFail(@"Expectation failed: %@", error);
     }];
+}
+
+- (void)test010_STE_89__streams_compatibility {
+    NSString *privateKeyString = self.utils.streamsCompatibilityDict[@"private_key"];
+    NSString *encryptedString = self.utils.streamsCompatibilityDict[@"encrypted_data"];
+    NSString *originString = self.utils.streamsCompatibilityDict[@"origin_data"];
+    NSData *privateKeyData = [[NSData alloc] initWithBase64EncodedString:privateKeyString options:0];
+    NSData *encryptedData = [[NSData alloc] initWithBase64EncodedString:encryptedString options:0];
+    NSData *originData = [[NSData alloc] initWithBase64EncodedString:originString options:0];
+
+    NSError *error;
+    VSMVirgilKeyPair *keyPair = [self.utils.crypto importPrivateKeyFrom:privateKeyData error:&error];
+    XCTAssert(error == nil);
+
+    VTEEThree* ethree = [self.utils setupDeviceWithIdentity:nil
+                                                    keyPair:keyPair
+                                                keyPairType:VSMKeyPairTypeEd25519
+                                                   register:false error:&error];
+    XCTAssert(error == nil);
+
+    NSInputStream *inputStream = [[NSInputStream alloc] initWithData:encryptedData];
+    NSOutputStream *outputStream = [[NSOutputStream alloc] initToMemory];
+
+    [ethree authDecrypt:inputStream to:outputStream from:nil error:&error];
+    XCTAssert(error == nil);
+
+    NSData *decryptedData = [outputStream propertyForKey:NSStreamDataWrittenToMemoryStreamKey];
+
+    XCTAssert([originData isEqualToData:decryptedData]);
 }
 
 @end
