617580c9036f6e930f19568408943d7720a14db6
SergeySeroshtan <sergey.seroshtan@gmail.com>
2020-08-13 11:14:20 +0300

    Use XCTAssertGreaterThan instead of XCTAssert where appropriate

--- Tests/VTE007_Benchmarks.swift
+++ Tests/VTE007_Benchmarks.swift
@@ -68,7 +68,7 @@ class VTE007_Benchmarks: XCTestCase {
         print()
 
         if let maxTime = maxTime {
-            XCTAssert(maxTime > average)
+            XCTAssertGreaterThan(maxTime, average)
         }
     }
 
