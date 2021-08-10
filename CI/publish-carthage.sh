5d39a2ec438ce3f142e62a2dbadd59599457947b
Yevhen Pyvovarov <evgenypyvovarov@gmail.com>
2021-08-10 22:47:54 +0300

    Fixed ci deploy

--- CI/publish-carthage.sh
+++ CI/publish-carthage.sh
@@ -3,6 +3,4 @@ brew outdated carthage || brew upgrade carthage;
 carthage build --use-xcframeworks --no-skip-current;
 
 # TODO: Should be replaced by carthage archive, when it supports xcframeworks
-FRAMEWORKS_PATH=Carthage/Build
-find ${FRAMEWORKS_PATH} ! -name 'VirgilE3Kit.xcframework' -delete
-zip -r VirgilE3Kit.xcframework.zip ${FRAMEWORKS_PATH}
+zip -r VirgilE3Kit.xcframework.zip Carthage/Build/VirgilE3Kit.xcframework
